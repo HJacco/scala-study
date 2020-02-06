a43c3fad176090c3add5dcaa3669ad37880938f3
cj.huang <cj.huang@ctrip.com>
2020-02-06 21:09:04 +0800

    chapter16

---
+++ src/main/scala/chapter16/XmlParserDemo.scala
@@ -0,0 +1,9 @@
+package chapter16
+
+
+object XmlParserDemo {
+  def main(args: Array[String]): Unit = {
+    var xml = <html><head><title>xml parse</title></head><body>Jacco's demo</body></html>
+    println(xml)
+  }
+}
